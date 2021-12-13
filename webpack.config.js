f81ae5c728e902c2b30a09804f8d32549e7990ad
zhangmingdi <3201896772@qq.com>
2021-12-13 23:17:15 +0800

    初始化

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist')
+    },
+    module: {
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            filename: 'index.html'
+        })
+    ],
+}
